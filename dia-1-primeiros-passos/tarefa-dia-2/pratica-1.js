d0d471bf94d63564927636a608d59774143cf6f4
dbrdark <brunomattos202030@hotmail.com>
2022-08-24 17:49:27 -0300

    dia 2

---
+++ dia-1-primeiros-passos/tarefa-dia-2/pratica-1.js
@@ -0,0 +1,8 @@
+let nome = "nome: Bruno"
+console.log(nome)
+let email = "email: brunomattos202030@hotmail.com"
+console.log(email)
+let profissão = "profissão: Freelance"
+console.log(profissão)
+let lugar = "lugar: Canada"
+console.log(lugar)
