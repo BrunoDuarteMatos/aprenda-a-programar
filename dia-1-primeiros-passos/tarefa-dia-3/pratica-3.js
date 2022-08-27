49a9171df81317a6d18c18fb1283ef2029e9317f
dbrdark <brunomattos202030@hotmail.com>
2022-08-27 12:25:17 -0300

    tarefas dia 3

---
+++ dia-1-primeiros-passos/tarefa-dia-3/pratica-3.js
@@ -0,0 +1,8 @@
+var numeros=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+];
+for(var i=0; i < numeros.length; i++){
+    var numero= numeros[i];
+    if ((numero %2)!= 0){
+        console.log (numero)
+    }
+}
